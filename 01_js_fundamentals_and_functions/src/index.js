14702729bfdab14c777c5a1bbdec86c2a6dd95e2
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-10-07 15:36:56 -0700

    ADD: js fundamentals and functions starter

---
+++ 01_js_fundamentals_and_functions/src/index.js
@@ -0,0 +1,21 @@
+//✅ Data Types
+
+//✅ Variables
+
+//✅ Equality Operators
+
+//✅ Comparison Operators
+
+//✅ Logical Operators
+
+//✅ control flow
+
+//✅ Create hello world using REGULAR functions
+
+//✅ Global Scope
+
+//✅ Function Scope
+
+//✅ Block Scope
+
+//✅ BONUS: higher-order functions
