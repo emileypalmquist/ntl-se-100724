9c5b48888baa6b683ed0becc56644ed56854dd8d
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-10-08 08:37:42 -0700

    ADD: objects, arrays, iteration lecture starter code

---
+++ 02_arrays_objects_iteration/objects.js
@@ -0,0 +1,44 @@
+// ✅ Creating & Accessing Objects
+
+
+// creating object literals
+//   empty object (no key / value pairs)
+
+//   object with single property (one key / value pair)
+   
+//   object with multiple properties of different data types (multiple key / value pairs)
+    
+// accessing object properties
+//   bracket notation (in what situations would we need to use this?)
+
+//   dot notation (better for readability)
+
+//   convert object properties and values into an array
+
+// ✅ Modifying Objects
+
+    // destructively
+
+        //   adding a property
+
+        //   changing a property value
+
+        //   removing a property
+            
+
+    // nondestructively
+        // use the spread operator {...} to create a clone of the original object...
+        //   ...that you can then modify without altering the original object
+        // dogCopy.age = 2
+
+
+// ✅ Iterating Over Objects vs. Arrays
+
+// what is the main distinction between looping and iterating?
+
+//   for...in (iterate over keys of an object)
+
+//   for...of (iterate over values of an iterable object (e.g., array, string, etc.))
+
+
+
