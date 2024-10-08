48075fb7af38e4c40605630270c8d2b7d3488124
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-10-08 12:21:20 -0700

    DONE: objects, arrays, iteration lecture code

--- 02_arrays_objects_iteration/arrays.js
+++ 02_arrays_objects_iteration/arrays.js
@@ -1,32 +1,54 @@
 // ✅ Creating / Accessing Arrays
 
 //   what are arrays?
-
+// collections of data/ lists
 //   define an array with several values
+const firstArry = [1, 'hello', {name: 'Emiley'}]
+          //  0     1       2
+firstArry[1] = "hello world"
 //   can contain a mix of data types
 //   what is the technical term for these individual items?
+// element
 //   count items in an array
+// console.log(firstArry.length)
 
 // ✅ Modifying Arrays
 
 // adding elements to an array
 //    add element to end of array
 
-// add element to beginning of array
+firstArry.push('new element', 1000, {})
 
+// add element to beginning of array
+firstArry.unshift(100, 'hello', [])
 // removing elements from an array
 //   remove last element and return that element
 
-// ✅ Iterating Through Arrays
+firstArry.pop()
+// look into: splice, slice
 
+// ✅ Iterating Through Arrays
+const dogs = ['Golden Retriever', 'Yorkie', 'Poodle', 'Maltese']
 //   looping over arrays
 
 //   for loop
+for (let i = 0; i < dogs.length; i++) {
+    console.log(`One dog breed is ${dogs[i]}`)
+}
 
 //   for...of loop
 
-//   while loop
+for (const dogBreed of dogs) {
+    console.log(`One dog breed is ${dogBreed}`)
+}
 
+//   while loop
+let i = 0
+
+while (i < dogs.length) {
+    console.log(dogs[i])
+    i++
+}
 
 
 
--- 02_arrays_objects_iteration/objects.js
+++ 02_arrays_objects_iteration/objects.js
@@ -1,44 +1,82 @@
 // ✅ Creating & Accessing Objects
-
-
 // creating object literals
 //   empty object (no key / value pairs)
+const firstObj = {}
 
 //   object with single property (one key / value pair)
-   
+const secondObj = {
+    name: "Emiley"
+}
 //   object with multiple properties of different data types (multiple key / value pairs)
-    
+const dog = {
+    name: 'Apollo',
+    age: 2,
+    tricks: ['sit', 'down', 'stay'],
+    sit: () => {
+        console.log('sit')
+        return 'good boy'
+    }
+}
+
 // accessing object properties
 //   bracket notation (in what situations would we need to use this?)
-
+// console.log(dog['name'])
 //   dot notation (better for readability)
-
+// console.log(dog.sit())
 //   convert object properties and values into an array
-
+// console.log(Object.entries(dog))
+// Object.keys(dog)
+// Object.values(dog)
 // ✅ Modifying Objects
 
     // destructively
-
         //   adding a property
-
+        dog.breed = 'Golden Retriever'
+        // dog['breed'] = 'Golden Retriever'
         //   changing a property value
-
+        dog.age = 3
         //   removing a property
-            
+        delete dog.tricks
 
     // nondestructively
         // use the spread operator {...} to create a clone of the original object...
         //   ...that you can then modify without altering the original object
-        // dogCopy.age = 2
+        const copyDog = {...dog}
+        copyDog.name = 'Daisy'
 
 
 // ✅ Iterating Over Objects vs. Arrays
 
 // what is the main distinction between looping and iterating?
+// looping - counting to 60
+// iterating - printing out each element in an array
 
 //   for...in (iterate over keys of an object)
-
+for (const key in dog) {
+    console.log(dog[key])
+}
 //   for...of (iterate over values of an iterable object (e.g., array, string, etc.))
+for (let value of Object.values(dog)) {
+    console.log(value)
+}
 
+// for (let i=0; i< 5; i++) {
+//     for (let j=0; j<5; j++) {
+//         console.log(`j is ${j}`)
+//     }
+//     console.log(`i is at ${i}`)
+// }
 
+const grid = [
+    [0,0,0],
+    [1,1,1],
+    [2,2,2]
+]
 
+for (let i = 0; i < grid.length; i++) {
+    console.log(grid[i])
+    const row = grid[i]
+    for (let j=0; j< row.length; j++ ) {
+        console.log(row[j])
+    }
+}
