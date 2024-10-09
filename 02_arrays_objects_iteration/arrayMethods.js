d09b5da6499e8518042ff51ded86dd8963b58fa6
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-10-09 08:59:03 -0700

    DONE: array methods forEach, map, filter, find

--- 02_arrays_objects_iteration/arrayMethods.js
+++ 02_arrays_objects_iteration/arrayMethods.js
@@ -1,21 +1,58 @@
 // ✅ Array Iteration Methods
+// higher order function - a function that either excepts or returns a function
     // What are callback functions?
+    // a function that gets passed to another function as an argument
+    // gets called by that function
+
+    function callbackFunc() {
+        console.log('I was called')
+    }
+
+    function higherOrderFunc(anotherFunct) {
+        anotherFunct()
+    }
+
+    higherOrderFunc(callbackFunc)
+
+
     const dogs = [
-        {id: 1, name: 'Bailey', breed: 'Yorkie'},
+        {id: 1, name: 'Bailey', breed: 'Yorkie', tricks: []},
         {id: 2, name: 'Daisy', breed: 'Golden Retriever'}, 
-        {id: 3, name: 'Apollo', breed: 'Golden Retriever'}
+        {id: 3, name: 'Apollo', breed: 'Golden Retriever', tricks: []}
     ]
-    // .forEach()
+    // .forEach() - execute some prvided function for each element in the array
+    function executedForEachElementInTheArray(dogObj) {
+        console.log(`${dogObj.name} is a ${dogObj.breed}`)
+        if (!dogObj.tricks) {
+            dogObj.tricks = []
+        }
+        dogObj.tricks.push('sit')
+        if (dogObj.breed === 'Golden Retriever') {
+            dogObj.tricks.push('down')
+        }
+    }
+    dogs.forEach(executedForEachElementInTheArray)
+    console.log(dogs)
+    // dogs.forEach((dogObj) => console.log(`${dogObj.name} is a ${dogObj.breed}`))
         
     // .map()
-
+    // no curlies implicit return "name" () => "name"
+    // with curlies explicit return "name" 
+    // () => { 
+    //     return "name"
+    // }
+    const newArry = dogs.map((dogObj) => {
+        return dogObj.name
+    } )
+    console.log("map returned ", newArry)
 
     // .filter()
         // Shallow copy
-
+    const goldens = dogs.filter((dogObj) => dogObj.breed === 'Golden Retriever')
+    console.log(goldens)
     // .find()
-
-
+    const firstGolden = dogs.find((dogObj) => dogObj.breed === 'Golden Retriever')
+    console.log(firstGolden)
 
 // 🚧 Break Out Activity 1:
 const strings = ['hello', 'hi', 'world', 'potato']
