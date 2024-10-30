9ce6d2d0b4cf122454786c35ba517d454797fecd
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-10-30 11:12:07 -0700

    DONE: state and events lecture

--- 09_state_and_events/project_showcase/src/components/Header.js
+++ 09_state_and_events/project_showcase/src/components/Header.js
@@ -1,11 +1,24 @@
+import { useState } from 'react'
+
 function Header() {
+    const [darkMode, setDarkMode] = useState(true)
+
+    function handleDarkModeClick() {
+        setDarkMode((currentState) => !currentState)
+        // setDarkMode(!darkMode)
+    }
+
+    const buttonText = darkMode ? "Light Mode" : "Dark Mode"
+
     return (
         <header>
             <h1>
                 <span className="logo">{"//"}</span>
                 Project Showcase
             </h1>
-            <button>Dark Mode</button>
+            <button onClick={handleDarkModeClick}>
+                {buttonText}
+            </button>
         </header>
     )
 }
