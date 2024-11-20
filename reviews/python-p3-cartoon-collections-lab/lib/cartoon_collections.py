3e2a776081de7536ff6f39100a93cb8c273df056
Emiley Palmquist <emiley.palmquist@gmail.com>
2024-11-20 09:51:55 -0800

    DONE: review cartoon collections

--- reviews/python-p3-cartoon-collections-lab/lib/cartoon_collections.py
+++ reviews/python-p3-cartoon-collections-lab/lib/cartoon_collections.py
@@ -1,11 +1,30 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    i = 1
+    for name in names:
+        print(f"{i}. {name}")
+        i += 1
 
-def summon_captain_planet():
-    pass
+def format_call(call):
+    return f"{call.capitalize()}!"
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(calls):
+    # formatted_calls = []
+    # for call in calls:
+    #     formatted_calls.append(f"{call.capitalize()}!")
+    # return formatted_calls
 
-def find_the_cheese():
-    pass
+    # return [f"{call.capitalize()}!" for call in calls]
+    return [format_call(call) for call in calls]
+
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
+
+def find_the_cheese(snacks):
+    cheese = ["cheddar", "gouda", "camembert"]
+    for snack in snacks:
+        if snack in cheese:
+            return snack
+    return None
